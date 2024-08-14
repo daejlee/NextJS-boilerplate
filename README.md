2b31d0545bb6c4c6b489363f4fa6efc370acea60
daejlee <savif4991@gmail.com>
2024-08-14 14:49:05 +0900

    Feat: Added boiler plate

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # NextJS-boilerplate
-For faster kick-off
+
+For faster kick-off, followings are installed.
+
+- NextJS App Router
+- ESLint(airbnb-config)
+- Prettier
+- TailwindCSS
+- shadcn/ui
---
+++ boilerplate/src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Boilerplate',
+  description: 'Boilerplate for Next.js with Tailwind CSS and TypeScript',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en'>
+      <body className={inter.className}>
+        {/* Wrapped with main-content CSS. Check it in global.css */}
+        <div className='main-content'>{children}</div>
+      </body>
+    </html>
+  );
+}
