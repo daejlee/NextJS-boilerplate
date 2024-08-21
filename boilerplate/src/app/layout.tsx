fc3c35d1994c6bae11131f0735b27e9fd948193b
daejlee <savif4991@gmail.com>
2024-08-21 14:00:39 +0900

    Feat: Add shadcn basic components

--- boilerplate/src/app/layout.tsx
+++ boilerplate/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Toaster } from '@/components/ui/toaster';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body className={inter.className}>
         {/* Wrapped with main-content CSS. Check it in global.css */}
         <div className='main-content'>{children}</div>
+        <Toaster />
       </body>
     </html>
   );
